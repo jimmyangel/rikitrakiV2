13ff9d45ac1e89b3688d4db5864438e21fec417c
Ricardo Morin <morinricardo@gmail.com>
2026-01-21 12:49:08 -0800

    Move adaptive desc to Alpine plugin

--- src/types/alpine.d.ts
+++ src/types/alpine.d.ts
@@ -1,13 +1,19 @@
 import 'alpinejs'
 
 declare global {
+  // Declare the global Alpine variable
+  const Alpine: typeof import('alpinejs')
+
   interface Window {
     Alpine: typeof import('alpinejs')
   }
+}
 
-  // Extend Alpine's type to include .store()
-  namespace Alpine {
-    function store<T = any>(name: string, value?: T): T
+declare module 'alpinejs' {
+  interface Alpine {
+    plugin: (callback: (alpine: Alpine) => void) => void
+    data: (name: string, callback: (...args: any[]) => any) => void
+    store: <T = any>(name: string, value?: T) => T
   }
 }
 
