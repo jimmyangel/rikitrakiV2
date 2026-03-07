341a5c3b972e8803624856532cfa494bac6b22f3
Ricardo Morin <morinricardo@gmail.com>
2026-03-06 17:00:28 -0800

    Remove dead code

--- src/scripts/utils/revgeo/geohash2.js
+++ src/scripts/utils/revgeo/geohash2.js
@@ -1,41 +1,7 @@
 const BASE32 = '0123456789bcdefghjkmnpqrstuvwxyz'
 
 export function geohash2(lat, lon) {
-  let minLat = -90, maxLat = 90
-  let minLon = -180, maxLon = 180
-  let hash = ''
-  let even = true
-
-  while (hash.length < 2) {
-    if (even) {
-      const mid = (minLon + maxLon) / 2
-      if (lon >= mid) {
-        hash += BASE32[16]  // placeholder bit
-        minLon = mid
-      } else {
-        hash += BASE32[0]   // placeholder bit
-        maxLon = mid
-      }
-    } else {
-      const mid = (minLat + maxLat) / 2
-      if (lat >= mid) {
-        hash += BASE32[16]
-        minLat = mid
-      } else {
-        hash += BASE32[0]
-        maxLat = mid
-      }
-    }
-    even = !even
-  }
-
-  // Convert the two placeholder bits into real geohash chars
-  return encodeTwoChars(lat, lon)
-}
-
-function encodeTwoChars(lat, lon) {
-  // Full geohash encode, then slice first 2 chars
-  return encodeGeohash(lat, lon).slice(0, 2)
+  return encodeGeohash(lat, lon, 2)
 }
 
 function encodeGeohash(lat, lon, precision = 12) {
